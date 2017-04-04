a835f356c0eb8502bf8f184d2b2e533b6345dc06
E7 <jackzxty@126.com>
2017-04-04 18:57:58 +0800

    优化启动停止脚本

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+
+if [[ ! -z "${JAVA_HOME}" ]] ; then
+    export JAVA="${JAVA_HOME}/bin/java"
+fi
+
 export JETTY_BASE=`pwd`
 export JETTY_RUN="${JETTY_BASE}/run"
-(nohup ./bin/jetty.sh start &)
+(nohup ${JETTY_BASE}/bin/jetty.sh start &)
--- stop.sh
+++ stop.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+
+if [[ ! -z "${JAVA_HOME}" ]] ; then
+    export JAVA="${JAVA_HOME}/bin/java"
+fi
+
 export JETTY_BASE=`pwd`
 export JETTY_RUN="${JETTY_BASE}/run"
-./bin/jetty.sh stop
+${JETTY_BASE}/bin/jetty.sh stop
