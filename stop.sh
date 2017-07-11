d56d08053f937cd9322d547838d103098097a93c
李佳 <lijia@rl160.com>
2017-07-11 14:40:02 +0800

    优化启动停止脚本

--- stop.sh
+++ stop.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
-
 if [[ ! -z "${JAVA_HOME}" ]] ; then
     export JAVA="${JAVA_HOME}/bin/java"
 fi
 
-export JETTY_BASE=`pwd`
-export JETTY_RUN="${JETTY_BASE}/run"
-${JETTY_BASE}/bin/jetty.sh stop
+fullpath=$(realpath "${0}")
+JETTY_BASE=$(dirname "${fullpath}")
+JETTY_RUN="${JETTY_BASE}/run"
+export JETTY_BASE
+export JETTY_RUN
+
+"${JETTY_BASE}/bin/jetty.sh" stop
